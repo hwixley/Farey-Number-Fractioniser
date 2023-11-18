574ee6c74576066ef158e77d1da21c86fa76f0ae
Harry Wixley <hwixley1@gmail.com>
2023-11-18 21:42:11 +0000

    GPT-commit: Refactor test.py for improved functionality
    GPT-commit: Refactor test.py for improved functionality - Modified the test.py file to update functions and improve overall functionality.

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import sys
+from yaspin import yaspin
+import numpy as np
 
 args = sys.argv[1:]
 
 precision = int(args[0])
-num = float(args[1])
-decimal = num - int(num)
+num_str = args[1].split(".")
+num = int(num_str[0])
+decimal = int(num_str[1])
 
 left = (0, 1)
 right = (1, 1)
@@ -12,11 +15,30 @@ right = (1, 1)
 def mediant(a, b):
     return (a[0] + b[0], a[1] + b[1])
 
-for _ in range(precision):
-    med = mediant(left, right)
-    if med[0] / med[1] < decimal:
-        left = med
-    else:
-        right = med
 
-print(f"{left[0]}/{left[1]}")
+with yaspin(text="Calculating", color="yellow") as spinner:
+    while True:
+        med = mediant(left, right)
+        if med[0] / med[1] < float(f"0.{decimal}"):
+            left = med
+        else:
+            right = med
+
+        print((med[0] / med[1]) - float(f"0.{decimal}"))
+        print(f"Calculating {med[0]}/{med[1]}")
+
+        print("")
+
+        if precision == 0:
+            break
+        else:
+            a = str(med[0] / med[1])[2:2+precision]
+            b = str(decimal)[:precision]
+            min_len = np.min([len(a), len(b)])
+            # print(a, b, min_len)
+            if a == b or (len(b) < len(a) and a[:min_len] == b[:min_len]):
+                break
+
+        
+
+print(f"{int(num)} + {med[0]}/{med[1]} \t OR \t {med[0] + int(num) * med[1]}/{med[1]}")
